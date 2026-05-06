5f86db580334c4e6e5f623af5d60bb299971fba6
sufianWG <webgurumrmbd@gmail.com>
2026-05-06 11:34:28 +0600

    added an new line as test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <div>
         <h1 className='text-6xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, sint!</h1>
+        <h2>Test github commit & push</h2>
       </div>
     </>
   )
