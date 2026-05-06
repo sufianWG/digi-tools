868ecd35b59657496e46e68c306a97f5b79a6ed8
sufianWG <webgurumrmbd@gmail.com>
2026-05-06 16:50:28 +0600

    improved the OutlineGradientBtn component

--- src/OutlineGradientBtn/OutlineGradientBtn.jsx
+++ src/OutlineGradientBtn/OutlineGradientBtn.jsx
@@ -1,7 +1,7 @@
 const OutlineGradientBtn = ({btnIcon = null, btnText = ""}) => {
     return (
-        <div className="cursor-pointer inline-block items-center font-medium rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-[2px]">
-            <button className="btn cursor-pointer bg-white text-base p-1 rounded-full text-black flex gap-2"> {btnIcon} {btnText}</button>
+        <div className="cursor-pointer inline-block items-center font-medium rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-[1px]">
+            <button className="btn cursor-pointer bg-white text-base py-1 px-3 rounded-full text-black flex gap-2"> {btnIcon} {btnText}</button>
         </div>
     );
 };
