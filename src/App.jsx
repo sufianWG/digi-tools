487e8085f9a3bb0bfe13e8640235a79dfaee0430
sufianWG <webgurumrmbd@gmail.com>
2026-05-06 11:32:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-6xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, sint!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
